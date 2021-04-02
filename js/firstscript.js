48766f68f7b17278da070221a086ce4ab9329e83
MLA <mla@MLAs-MBP.fios-router.home>
2021-04-02 18:03:18 -0400

    Got the URL working

--- js/firstscript.js
+++ js/firstscript.js
@@ -1,3 +1,6 @@
 const root = document.querySelector(".site-wrap");
-const nytapi = "Aeh0sFGzxJYrFQF0F00wI8HSdL0RtDtN";
+const nytapi = "qa1UnMV3WkBkjX7RL1f8lJYTd9J86fPT";
 const nytUrl = `https://api.nytimes.com/svc/movies/v2/reviews/picks.json?api-key=${nytapi}`;
+
+const just_url = document.querySelector(".API_URL")
+just_url.innerHTML = `<a href="${nytUrl}">LINK</a>`
