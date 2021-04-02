8c9fd7695db43c4204d18462fffe4f8ba9485a6c
MLA <mla@MLAs-MBP.fios-router.home>
2021-04-02 17:30:18 -0400

    Starting out

---
+++ js/firstscript.js
@@ -0,0 +1,3 @@
+const root = document.querySelector(".site-wrap");
+const nytapi = "Aeh0sFGzxJYrFQF0F00wI8HSdL0RtDtN";
+const nytUrl = `https://api.nytimes.com/svc/movies/v2/reviews/picks.json?api-key=${nytapi}`;
