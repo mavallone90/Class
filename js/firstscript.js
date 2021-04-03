48eb1b3e391e8cd6800f4e585ea106b728fcf146
MLA <mla@MLAs-MBP.fios-router.home>
2021-04-02 20:26:36 -0400

    Got the titles, author and rating to display

--- js/firstscript.js
+++ js/firstscript.js
@@ -1,6 +1,35 @@
 const root = document.querySelector(".site-wrap");
 const nytapi = "qa1UnMV3WkBkjX7RL1f8lJYTd9J86fPT";
-const nytUrl = `https://api.nytimes.com/svc/movies/v2/reviews/picks.json?api-key=${nytapi}`;
+const type = "all";
+const movieStuff = `https://api.nytimes.com/svc/movies/v2/reviews/${type}.json?api-key=${nytapi}`;
 
-const just_url = document.querySelector(".API_URL")
-just_url.innerHTML = `<a href="${nytUrl}">LINK</a>`
+const just_url = document.querySelector(".API_URL");
+just_url.innerHTML = `<a href="${movieStuff}">LINK</a>`;
+
+// const stuff = document.querySelector(".stuff");
+// stuff.innerHTML = movieStuff.map( function(item){return item.status})
+
+// just getting the results in the console
+// fetch(movieStuff)
+//     .then((response) => response.json())
+//     .then((myJson) => console.log(myJson.results));
+
+// passing to function
+fetch(movieStuff)
+    .then((response) => response.json())
+    .then((myJson) => renderStories(myJson));
+
+function renderStories(data) {
+    data.results.forEach(function (story) {
+        // console.log(story)
+        var theArticles = document.createElement("div");
+        theArticles.className = 'movietalk';
+        theArticles.innerHTML = 
+            `<h2>${story.display_title}</h2>
+            <h3>Review By ${story.byline}</h3>
+            <h3>Rated ${story.mpaa_rating}</h3>`
+        ;
+        // console.log(theArticles);
+        just_url.append(theArticles);
+    })
+};
